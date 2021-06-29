41a75383fb6ae58164ca59cd1ef0813bfbd07e4b
JoshWiegman2004 <62081011+JoshWiegman2004@users.noreply.github.com>
2021-06-30 09:19:08 +1200

    Created Popup
    Created popup screen for entering a class name, allowing me to start linking it to the backend.

--- lms_system/classes.php
+++ lms_system/classes.php
@@ -1,11 +1,12 @@
-
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Classes - Techwise</title>
-    <meta name="description" content="Video tutorials for those who need them. Set up all kinds of social media and video chatting programs through easy video tutorials! ">
+    <meta name="description"
+        content="Video tutorials for those who need them. Set up all kinds of social media and video chatting programs through easy video tutorials! ">
     <link rel="icon" type="image/png" sizes="16x8" href="assets/img/rsz_2326b559-520a-4bff-ad0c-f585ab4b1ddc_1.png">
     <link rel="icon" type="image/png" sizes="32x18" href="assets/img/rsz_2326b559-520a-4bff-ad0c-f585ab4b1ddc.png">
     <link rel="icon" type="image/png" sizes="1025x575" href="assets/img/2326b559-520a-4bff-ad0c-f585ab4b1ddc.png">
@@ -13,12 +14,12 @@
     <link rel="icon" type="image/png" sizes="1025x575" href="assets/img/2326b559-520a-4bff-ad0c-f585ab4b1ddc.png">
     <link rel="stylesheet" href="../assets/css/classes.css">
 </head>
-    
+
 <body>
     <?php
     include 'header.php';
     ?>
-    
+
     <div class="classes-container">
         <div class="classes-card">
             <div class="classes-box">
@@ -31,10 +32,29 @@
             </div>
         </div>
     </div>
-    
+
     <div class="test">
-        <button class="classes-plus-icon">+</button>
-    </div>    
+        <button class="classes-plus-icon" onclick="openForm()">+</button>
+    </div>
+    <div class="form-popup" id="addClass">
+        <form action="/classes.php" class="form-container">
+            <label for="psw"><b>Invite Code</b></label>
+            <input type="text" placeholder="Enter class code" name="classCode" required>
+
+            <button type="submit" class="btn">✓</button>
+            <button type="submit" class="btn cancel" onclick="closeForm()">✖</button>
+        </form>
+    </div>
 </body>
+
+<script>
+function openForm() {
+    document.getElementById("addClass").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("addClass").style.display = "none";
+}
+</script>
 
 </html>
--- lms_system/index.php
+++ lms_system/index.php
@@ -1,35 +1,37 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <link rel="apple-touch-icon" sizes="180x180" href="../assets/img/favicons/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="../assets/img/favicons/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="../assets/img/favicons/favicon-16x16.png">
-        <link rel="manifest" href="../assets/img/favicons/site.webmanifest">
-        <link rel="mask-icon" href="../assets/img/favicons/safari-pinned-tab.svg" color="#000000">
-        <meta name="msapplication-TileColor" content="#2d89ef">
-        <meta name="theme-color" content="#ffffff">
-        <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" href="../assets/css/dark-mode.css">
-    </head>
 
-    <body>
+<head>
+    <link rel="apple-touch-icon" sizes="180x180" href="../assets/img/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../assets/img/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../assets/img/favicons/favicon-16x16.png">
+    <link rel="manifest" href="../assets/img/favicons/site.webmanifest">
+    <link rel="mask-icon" href="../assets/img/favicons/safari-pinned-tab.svg" color="#000000">
+    <meta name="msapplication-TileColor" content="#2d89ef">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/dark-mode.css">
+</head>
+
+<body>
     <!__Video background__>
         <video autoplay muted loop id="backVid">
             <source src="../assets/img/video.mp4" type="video/mp4">
         </video>
-    <!__Content_Main__>
-        <section>
-            <p>test123</p>
-        </section>
-    <!__Dark mode__>
-        <!__Dark mode toggle button__>
-        <button onclick="darklight()">Toggle dark mode</button>
-        <!__Dark mode script__>
-        <script>
-            function darklight() {
-                var element = document.body;
-                element.classList.toggle("dark-mode");
-            }
-        </script>
-    </body>
+        <!__Content_Main__>
+            <section>
+                <p>test123</p>
+            </section>
+            <!__Dark mode__>
+                <!__Dark mode toggle button__>
+                    <button onclick="darklight()">Toggle dark mode</button>
+                    <!__Dark mode script__>
+                        <script>
+                        function darklight() {
+                            var element = document.body;
+                            element.classList.toggle("dark-mode");
+                        }
+                        </script>
+</body>
+
 </html>
